bc0783566d5129b03580b7040d4f8f761e6f878f
SweckEUW <SweckEUW@users.noreply.github.com>
2023-01-02 17:52:38 +0000

    deploy: 06e92301ce0f7bc19f61679a92ace49766f80826

---
+++ assets/About-05255738.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as o}from"./index-7dbf9771.js";const r={data:()=>({})};function c(n,a,s,_,p,u){return e(),o("div",null," About ")}const f=t(r,[["render",c]]);export{f as default};
