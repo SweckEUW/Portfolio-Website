b06a99b91ff3be60e40e51fb7e647f3cd7bbb491
SweckEUW <SweckEUW@users.noreply.github.com>
2023-01-09 18:40:01 +0000

    deploy: 7b3d5c62f3ca2b6ba58480638acca70b9f8d863b

--- assets/About-a6dba847.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as o}from"./index-b396d1c8.js";const r={data:()=>({})};function c(n,a,s,_,p,u){return e(),o("div",null," About ")}const f=t(r,[["render",c]]);export{f as default};
---
+++ assets/Contact-937229de.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as o}from"./index-5c9329de.js";const c={data:()=>({})};function n(a,r,s,_,p,d){return e(),o("div",null," Contact ")}const l=t(c,[["render",n]]);export{l as default};
