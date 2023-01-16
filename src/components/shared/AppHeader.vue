8df1eed58f5aff94c3f9e5db0ceaab9696b2523c
Simon Weck <Simon.weck@raumtaenzer.com>
2023-01-16 01:49:23 +0100

    minor adjustments navbar

--- src/components/shared/AppHeader.vue
+++ src/components/shared/AppHeader.vue
@@ -2,7 +2,7 @@
 	<div class="AppHeader">
 
 		<div class="ap-left">
-			<h1 @click="$router.push('/')">[Simon Weck]</h1>
+			<h1 @click="goToHome()">[Simon Weck]</h1>
 		</div>
 		
 		<div class="ap-right">
@@ -16,9 +16,17 @@
 
 <script>
 export default {
-	data: ()=>({
-       
-    }),
+	methods:{
+		goToHome(){
+			if(this.$route.path == "/"){
+				document.getElementsByTagName("html")[0].style.scrollBehavior = "smooth";
+				setTimeout(() => {window.scrollTo(0, 0); }, 0);
+				setTimeout(() => { document.getElementsByTagName("html")[0].style.scrollBehavior = ""; }, 1000);
+			}else{
+				this.$router.push('/');
+			}
+		}
+	}
 };
 </script>
 
@@ -28,7 +36,7 @@ export default {
 	z-index: 99;
 	padding: 20px 0px;
 	position: fixed;
-	top: 0;
+	top: 30px;
 	filter: drop-shadow(0px 0px 100px rgba(255, 255, 255, 0.295));
 	margin: 0 5%;
 }
@@ -40,8 +48,8 @@ export default {
 }
 h1{
 	display: inline-block;
-	font-size: 30px;
-	margin: 0 20px;
+	font-size: 26px;
+	margin: 0 15px;
 	cursor: pointer;
 }
 h1:first-child{
