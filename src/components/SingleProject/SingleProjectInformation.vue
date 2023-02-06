b16268b2c07ae29d48c0c5625ad716d7b40eb469
Simon Weck <Simon.weck@raumtaenzer.com>
2023-02-06 02:32:11 +0100

    added icons for tools/languages

--- src/components/SingleProject/SingleProjectInformation.vue
+++ src/components/SingleProject/SingleProjectInformation.vue
@@ -3,13 +3,14 @@
 		<div class="spi-description">
 			<h1>Description</h1>
 			<div>{{project.description_long}}</div>
-			<h1>Tools</h1>
+			<h1>Tools/Languages</h1>
+			<img v-for="tool in project.tools" :key="tool" :title="tool" :src="getImageFromTool(tool)" alt="">
 		</div>
 		<div class="spi-sideInformation">
 			<h1>Role</h1>
 			<div v-for="role in project.roles" :key="role">{{role}}</div>
 			<h1>Type</h1>
-			<div v-for="type in project.types" :key="type">{{type}}</div>
+			<div v-for="pType in project.types" :key="pType">{{pType}}</div>
 			<h1>Created</h1>
 			<div>{{project.created}}</div>
 		</div>
@@ -19,9 +20,11 @@
 <script>
 export default {
 	props: ['project'],
-    data: ()=>({
-		
-    }),
+	methods: {
+		getImageFromTool(tool){
+			return new URL(`/src/assets/icons/${tool}.png`, import.meta.url);
+		}
+	}
 };
 </script>
 
@@ -49,6 +52,17 @@ h1{
 	margin-bottom: 10px;
 	margin-top: 20px;
 	text-shadow: none;
+}
+img{
+	display: inline-block;
+	width: 60px;
+	height: 60px;
+	object-fit: contain;
+	margin: 0 15px;
+	filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.2));
+}
+img:first-of-type{
+	margin-left: 0px;
 }
 
 /*MOBILE*/
