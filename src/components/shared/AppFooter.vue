31221d0a165ff205207573658ea10d83f564a582
Simon Weck <Simon.weck@raumtaenzer.com>
2023-02-06 02:31:05 +0100

    added social links

--- src/components/shared/AppFooter.vue
+++ src/components/shared/AppFooter.vue
@@ -1,16 +1,29 @@
 <template>
 	<div class="AppFooter">
-		<p>@2023 Simon Weck</p>
-		<p>|</p>
-		<p class="af-link" @click="$router.push('/Imprint')">Imprint/Privacy Policy</p>
+
+		<div class="af-socials">
+			<img :src="getIcon('LinkedIn')" titl="LinkedIn" @click="openLink('https://www.linkedin.com/in/sweck/')" alt="">
+			<img :src="getIcon('GitHub')" titl="GitHub" @click="openLink('https://github.com/SweckEUW/')" alt="">
+		</div>
+		
+		<div class="af-imprint">
+			<p>@2023 Simon Weck</p>
+			<p>|</p>
+			<p class="af-link" @click="$router.push('/Imprint')">Imprint/Privacy Policy</p>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	data: ()=>({
-       
-    }),
+	methods: {
+		getIcon(icon){
+			return new URL(`/src/assets/icons/${icon}.png`, import.meta.url);
+		},
+		openLink(link){
+			window.open(link, '_blank').focus();
+		}
+	}
 };
 </script>
 
@@ -20,10 +33,31 @@ export default {
 	background: rgba(0, 0, 0, 0.3);
 	text-align: center;
 	color: white;
-	height: 80px;
+	padding: 20px;
+	font-size: 18px;
+}
+
+.af-socials{
+	margin-bottom: 20px;
+}
+img{
+	width: 40px;
+	height: 40px;
+	object-fit: contain;
+	filter: brightness(0%) invert(1);
+	margin: 0px 12px;
+	cursor: pointer;
+	transition: .3s filter ease;
+}
+img:hover{
+	filter: brightness(0%) invert(0.9);
+}
+
+
+
+.af-imprint{
 	display: flex;
 	justify-content: center;
-	font-size: 18px;
 }
 p{
 	display: inline-block;
