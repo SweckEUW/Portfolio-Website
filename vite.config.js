71ef01c55e98e8963457c2a6209e99e3183d3df5
Simon Weck <Simon.weck@raumtaenzer.com>
2023-02-28 23:24:03 +0100

    adjusted build for 404 page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import path from 'path'
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -12,5 +14,13 @@ export default defineConfig({
   },
   server: {
     host: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, '404.html')
+      }
+    }
   }
 })
