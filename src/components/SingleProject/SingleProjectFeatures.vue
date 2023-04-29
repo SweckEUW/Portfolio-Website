5b14f91565268c6060b9e6c8a07f228daf3fce5a
SweckEUW <simon.weck98@yahoo.de>
2023-04-29 05:22:00 +0200

    added image enlarging

--- src/components/SingleProject/SingleProjectFeatures.vue
+++ src/components/SingleProject/SingleProjectFeatures.vue
@@ -10,8 +10,18 @@
 		</div>
 		
 		<div class="spf-headerImages">
-			<img v-for="image in project.headerImages" :key="image" :src="getMedia('pictures/'+image)" alt="">
+			<div class="spf-headerImages-container" v-for="image in project.headerImages" :key="image" >
+				<img class="spf-headerImages-image" :src="getMedia('pictures/'+image)" alt="" @click="openImage(image)">
+			</div>
+			
 		</div>
+
+		<transition name="fade" mode="out-in">
+			<div class="sp-enlarged" v-show="enlargeImage" @click="closeImage()">
+				<div class="close" @click="closeImage()"/>
+				<img :src="getMedia('pictures/'+enlargedImagePath)" alt="">
+			</div>
+		</transition>
 
 	</div>
 </template>
@@ -22,9 +32,26 @@ import VideoWithButton from '@/components/shared/VideoWithButton.vue';
 export default {
 	components: {VideoWithButton},
 	props: ['project'],
+	data: ()=>({
+        enlargeImage: false,
+		enlargedImagePath: ""
+    }),
 	methods: {
 		getMedia(media){
 			return new URL(`/src/assets/projects/${this.project.folder}/${media}`, import.meta.url);
+		},
+		openImage(image){
+			this.enlargedImagePath = image;
+			this.enlargeImage = true;
+
+			var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+			document.getElementsByClassName("sp-enlarged")[0].style.top = scrollTop + "px";
+
+			window.onscroll = function () { window.scrollTo(0, scrollTop); };
+		},
+		closeImage(){
+			this.enlargeImage = false;
+			window.onscroll = undefined;
 		}
 	}
 };
@@ -69,13 +96,68 @@ export default {
 	justify-content: center;
 	margin-bottom: 100px;
 }
-.spf-headerImages img{
+.spf-headerImages-container{
 	width: 33%;
 	padding-right: 20px;
+	cursor: pointer;
+	overflow: hidden;
+}
+.spf-headerImages-image{
+	width: 100%;
+	height: 100%;
+	transition: .3s transform ease;
 	object-fit: cover;
+}
+.spf-headerImages-image:hover{
+	transform: scale(1.02);
 }
 .spf-headerImages img:last-of-type{
 	padding-right: 0px;
+}
+
+/* Pop-Up Image */
+.sp-enlarged{
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	z-index: 999;
+	left: 0;
+	background: rgba(0, 0, 0, 0.9);
+	display: flex;
+	justify-content: center;
+    align-items: center;
+}
+.sp-enlarged img{
+	width: 90%; 
+	height: 90%; 
+	object-fit: contain;
+}
+
+.close{
+  position: absolute;
+  right: 40px;
+  top: 40px;
+  width: 32px;
+  height: 32px;
+  opacity: 0.8;
+  cursor: pointer;
+}
+.close:hover {
+  opacity: 1;
+}
+.close:before, .close:after {
+  position: absolute;
+  left: 15px;
+  content: ' ';
+  height: 33px;
+  width: 4px;
+  background-color: #a30000;
+}
+.close:before {
+  transform: rotate(45deg);
+}
+.close:after {
+  transform: rotate(-45deg);
 }
 
 /*MOBILE*/
